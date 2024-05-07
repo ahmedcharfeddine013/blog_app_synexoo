c0da493a496d2f6063c6548c0f6b8b0405602416
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-07 17:31:49 +0100

    new-password reset action

---
+++ src/actions/new-password.ts
@@ -0,0 +1,50 @@
+"use server"
+import * as z from "zod"
+import { NewPasswordSchema } from "@/schema"
+import { getPasswordResetTokenByToken } from "@/data/password-reset-token"
+import { getUserByEmail } from "@/data/user"
+import bcrypt from 'bcryptjs'
+import db from "@/db/db"
+export const newPassword = async(
+    values: z.infer<typeof NewPasswordSchema>,
+    token?: string | null
+) => {
+    if (!token) {
+        return {error: 'Missing Token!'}
+    }
+    const validatedFields = NewPasswordSchema.safeParse(values)
+
+    if (!validatedFields.success) {
+        return {error: 'Invalid Fields!'}
+    }
+
+    const {password} = validatedFields.data
+    const existingToken = await getPasswordResetTokenByToken(token)
+
+    if (!existingToken) {
+        return {error: 'Invalid Token!'}
+    }
+
+    const hadExpired = new Date(existingToken.expires) < new Date()
+    if (hadExpired) {
+        return {error: 'Token has expired!'}
+    }
+
+    const existingUser = await getUserByEmail(existingToken.email)
+
+    if (!existingUser) {
+        return {error: "Email does not exist!"}
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await db.user.update({
+        where: {id: existingUser.id},
+        data: {password: existingUser.id}
+    })
+
+    await db.passwordResetToken.delete({
+        where: {id: existingToken.id}
+    })
+
+    return {success: 'Password updated!'}
+}
