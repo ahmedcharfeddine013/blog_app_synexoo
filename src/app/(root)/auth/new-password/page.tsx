51d5816ab97293d812d3cea120e5dc88d2952b16
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-07 16:42:52 +0100

    new password page

---
+++ src/app/(root)/auth/new-password/page.tsx
@@ -0,0 +1,13 @@
+
+import NewPasswordForm from '@/components/auth/NewPasswordForm'
+import React from 'react'
+
+const NewPasswordPage = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <NewPasswordForm />
+    </div>
+  )
+}
+
+export default NewPasswordPage
---
+++ src/components/auth/NewPasswordForm.tsx
@@ -0,0 +1,105 @@
+"use client";
+import * as z from "zod";
+import React, { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NewPasswordSchema } from "@/schema";
+import { useSearchParams } from "next/navigation";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { useTransition } from "react";
+import Link from "next/link";
+
+const NewPasswordForm = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    // setError("");
+    // setSuccess("");
+    // startTransition(() => {
+    //   newPassword(values, token).then((data) => {
+    //     setError(data?.error);
+    //     // TODO : Add when we add 2FA
+    //     setSuccess(data?.success);
+    //   });
+    // });
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center p-12 gap-6 border-2 rounded-xl">
+        <h1 className="text-xl">Enter a new password</h1>
+        <div className="flex flex-col w-[400px] gap-6">
+          <Form {...form}>
+            <form
+              className="flex flex-col items-start justify-center gap-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <div className="flex flex-col items-start justify-center gap-4 w-full">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="******"
+                          type="password"
+                          disabled={isPending}
+                        ></Input>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormError message={error} />
+              <FormSuccess message={success} />
+              <Button
+                type="submit"
+                className="w-full flex items-center justify-center gap-2"
+                disabled={isPending}
+              >
+                {" "}
+                Reset password
+              </Button>
+            </form>
+          </Form>
+          <Link
+            href="../auth/register"
+            className="px-8 text-center text-sm text-muted-foreground hover:underline"
+          >
+            Don&apos;t have an account?
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewPasswordForm;
