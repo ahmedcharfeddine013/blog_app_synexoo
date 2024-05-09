85fd135ea2a4ce5ad520fbfdf33485fe5cf90258
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-09 20:46:19 +0100

    sign in page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,34 @@
-import { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import db from "@/db/db";
 
-export const authOptions : NextAuthOptions ={
-    providers : [
-        CredentialsProvider({
-            name : 'Credentials',
-            credentials : {
-                email : {label : 'E-mail', type : 'text', placeholder : 'jleazh@gmail.com'},
-                password : {label : 'Password' , type : 'text'}
-            },
-            async authorize (credentials, ,req) {
-                const user 
-            }
-        })
-    ]
-}
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth/sign-in",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "smith", email: "azeby@gmail.com" };
+        if (user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
