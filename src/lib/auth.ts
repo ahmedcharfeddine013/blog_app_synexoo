15c4a57a73c7142071fea5533f289a5bbcc5fea6
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-09 20:17:05 +0100

    handler updated

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,17 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-export const handler : NextAuthOptions
+export const authOptions : NextAuthOptions ={
+    providers : [
+        CredentialsProvider({
+            name : 'Credentials',
+            credentials : {
+                email : {label : 'E-mail', type : 'text', placeholder : 'jleazh@gmail.com'},
+                password : {label : 'Password' , type : 'text'}
+            },
+            async authorize (credentials, ,req) {
+                const user 
+            }
+        })
+    ]
+}
