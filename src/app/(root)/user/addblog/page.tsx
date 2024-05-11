ed3eff902625e97aa838eb267ca57ea0c2e76023
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-11 02:21:59 +0100

    blog form

--- src/app/(root)/user/addblog/page.tsx
+++ src/app/(root)/user/addblog/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BlogForm from "../../../../components/blog/BlogForm";
 import { useCurrentUser } from "@/hooks/use-current-user";
-import AddBlogForm from "@/components/blog/AddBlogForm";
+import AddBlogForm from "@/components/blog/BlogForm";
 
 export default async function AddBlogUserPage() {
     const user = await useCurrentUser()
---
+++ src/components/blog/BlogForm.tsx
@@ -0,0 +1,75 @@
+import { Blog } from "@prisma/client";
+import React from "react";
+import { useFormState, useFormStatus } from "react-dom";
+import { AddBlog, updateBlog } from "./_actions/blog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+export default function BlogForm({ blog }: { blog?: Blog | null }) {
+  const [error, action] = useFormState(
+    blog == null ? AddBlog : updateBlog.bind(null, blog.id),
+    {}
+  );
+  return (
+    <form action={action}>
+      <div className="space-y-2">
+        <Label htmlFor="title">Name</Label>
+        <Input
+          type="text"
+          id="title"
+          name="title"
+          required
+          defaultValue={blog?.title || ""}
+        />
+        {blog?.title && <div className="text-red-500">{blog.title}</div>}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="summary">Name</Label>
+        <Input
+          type="text"
+          id="summary"
+          name="summary"
+          required
+          defaultValue={blog?.summary || ""}
+        />
+        {blog?.summary && <div className="text-red-500">{blog.summary}</div>}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="content">Name</Label>
+        <Input
+          type="text"
+          id="content"
+          name="content"
+          required
+          defaultValue={blog?.content || ""}
+        />
+        {blog?.content && <div className="text-red-500">{blog.content}</div>}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" required={blog == null} />
+        {blog != null && (
+          <Image
+            src={blog?.cover}
+            height="400"
+            width="400"
+            alt="Product Image"
+          />
+        )}
+        {blog?.cover && <div className="text-destructive">{blog.cover}</div>}
+      </div>
+      <SubmitButton />
+    </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Saving..." : "Save"}
+    </Button>
+  );
+}
